660ce0a5e56f59751ced2f418eb2323336caf2c8
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2020-05-26 12:33:20 -0400

    Update HW2.py

--- HW/HW2.py
+++ HW/HW2.py
@@ -1,14 +1,16 @@
-def triangle():
-    num = int(input('Enter the number of rows:      '))
-    for i in range(1, num + 1):
-        if i == 1:
-            x = 1
-            print(' ' * num, 'A')
-        elif i == num:
-            print(' ', 'A' * num * 2)
-        else:
-            print(' ' * (num - i + 1), 'A', ' ' * ((x * 2) - 2), 'A')
-            x += 1
+num = int(input('Give me the number of rows:        '))
+for i in range(num, num * 2 - 1):
+    currentline = [' '] * i
+    length = len(currentline) - 1
+    currentline[length] = 'A'
+    if i > num:
+        currentline[length - (2 * x)] = 'A'
+    else:
+        x = 0
+    finalline = ''
+    for letter in currentline:
+        finalline += letter
+    print(finalline)
+    x += 1
 
-
-triangle()
+print('A' * ((num * 2) - 1))
